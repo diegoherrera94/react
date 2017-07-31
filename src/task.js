742857bfc72c0034ada9b99d98f376bbd269059a
Diego Herrera <dherrera@jbknowledge.com>
2017-07-31 13:10:01 -0300

    niaca

---
+++ src/task.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Task extends React.Component{
+
+    static propTypes = {
+        name : React.PropTypes.string,
+        done : React.PropTypes.bool
+    };
+
+    static defaultProps = {
+        name : 'No task',
+        done : false
+    };
+    
+    render(){
+        return (
+            <li>
+                <input type="checkbox" checked={this.props.done ? 'checked' : null} />
+                {this.props.name}
+            </li>
+        )
+    }
+}
+
+export default Task;
